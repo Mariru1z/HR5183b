d53d39adbf0750a7196ae1ed875f41c998465f6c
Mariru1z <76128232+Mariru1z@users.noreply.github.com>
2024-10-06 09:32:04 -0600

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2085683.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 198566975, 198566976, 198566999, 198566979, 198566980, 198566981, 198566982, 198566983, 198566985, 198566986, 198566987, 198566988, 198566990, 198566991 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/198566996/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/198566997/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/198566995/1/ammo.js', 'preload' : true},
+];
